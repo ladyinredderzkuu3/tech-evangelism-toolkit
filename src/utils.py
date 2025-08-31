222b9814d20af11c0a4e5a49dc2132b0bf45372b
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-08-31 03:16:40 -0500

    feat: initial commit for src/utils.py

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+def load_resources():
+    return [
+        'Einführung in Python',
+        'Webentwicklung mit Flask',
+        'Datenvisualisierung mit Matplotlib'
+    ]
+
+
+def list_workshops():
+    return [
+        'Einführung in Programmierung',
+        'Cloud-Computing Grundlagen'
+    ]
